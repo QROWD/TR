82d615aba3e550901be9ce8f4e030555ca1e3afe
lpfgarcia <lpfgarcia@gmail.com>
2018-04-13 10:48:08 +0200

    add multiclass auc

--- exp/classifiers.r
+++ exp/classifiers.r
@@ -2,42 +2,61 @@
 # Luis P. F. Garcia 2018
 # Supervised Learning - Classifiers 
 
-ANN <- function(tran, test) {
+ANN <- function(train, test) {
 
-  model = MLP(class ~ ., tran, control=Weka_control(L=0.3, M=0.5))
-  pred = predict(model, test[,-ncol(test)], type="class")
+  model = mlp(class ~ ., train, control=RWeka::Weka_control(L=0.3, M=0.5))
+  pred = predict(model, test[,-ncol(test)], type="prob")
   names(pred) = row.names(test)
   return(pred)
-} 
+}
 
-C4.5 <- function(tran, test) {
-
-  model = RWeka::J48(class ~., tran)
-  pred = predict(model, test[,-ncol(test)], type="class")
-  names(pred) = rownames(test)
+C4.5 <- function(train, test) {
+  model = RWeka::J48(class ~ ., train)
+  pred = predict(model, test[,-ncol(test)], type="prob")
   return(pred)
 }
 
-kNN <- function(tran, test) {
-
-  pred = kknn(class ~., tran, test[,-ncol(test)], k=3)$fitted.values
-  names(pred) = rownames(test)
+kNN <- function(train, test, k=5) {
+  pred = kknn(class ~., train, test[,-ncol(test)], k=k)$prob
+  rownames(pred) = rownames(test)
   return(pred)
 }
 
-SVM <- function(tran, test) {
-
-  model = svm(class ~., tran, kernel="radial")
-  pred = predict(model, test[,-ncol(test)])
+LM <- function(train, test) {
+  model = svm(class ~ ., train, kernel="linear",  probability=TRUE)
+  pred = attr(predict(model, test[,-ncol(test)], probability=TRUE), "probabilities")
+  pred = pred[,levels(train$class)]
   return(pred)
 }
 
-accuracy <- function(pred, test) {
-  sum(diag(table(test$class, pred)))/sum(table(test$class, pred))
+NB <- function(train, test) {
+  model = naiveBayes(class ~ ., train)
+  pred = predict(model, test[,-ncol(test)], type="raw")
+  rownames(pred) = rownames(test)
+  return(pred)
 }
 
-classifiers <- function(tran, test) {
+RF <- function(train, test) {
+  model = randomForest(class ~ ., train)
+  pred = predict(model, test[,-ncol(test)], type="prob")
+   return(pred)
+}
+
+SVM <- function(train, test) {
+  model = svm(class ~ ., train, kernel="radial", probability=TRUE)
+  pred = attr(predict(model, test[,-ncol(test)], probability=TRUE), "probabilities")
+  pred = pred[,levels(train$class)]
+  return(pred)
+}
+
+multi.auc <- function(pred, class) {
+  pred = pred[cbind(seq_along(class), class)]
+  as.numeric(multiclass.roc(class, pred)$auc)
+}
+
+classifiers <- function(train, test) {
   sapply(CLASSIFIERS, function(c) {
-    accuracy(do.call(c, list(tran, test)), test)
+    pred = do.call(c, list(train, test))
+    multi.auc(pred, test$class)
   })
 }
