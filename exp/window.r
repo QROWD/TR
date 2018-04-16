272742001b8b3f0081c5544c1ae19f740c185b95
lpfgarcia <lpfgarcia@gmail.com>
2018-04-16 11:28:20 +0200

    move window functions

---
+++ exp/window.r
@@ -0,0 +1,26 @@
+# R code
+# Luis P. F. Garcia 2018
+# Split the time series as flat table
+
+ff <- function(data) {
+  aux = fft(data)/length(data)
+  sapply(aux, function(i) {
+    signif(Mod(i), 4)
+  })
+}
+
+index <- function(i, s) {
+  (((i-1)*s) + 1):(i*s)
+}
+
+window <- function(data, size=100) {
+
+  step = 1:(nrow(data)/size)
+  aux = t(sapply(step, function(i) {
+    ff(data[index(i, size), 1])
+  }))
+
+  aux = data.frame(aux)
+  aux$class = data[step*size, 2]
+  return(aux)
+}
