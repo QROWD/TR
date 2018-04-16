d9999005d1cc7e7c871036969d8737c47e7959c9
lpfgarcia <lpfgarcia@gmail.com>
2018-04-16 17:35:57 +0200

    export the model

---
+++ exp/extract.r
@@ -0,0 +1,20 @@
+# R code
+# Luis P. F. Garcia 2018
+# Extract the best model
+
+best <- function(result) {
+  clf = Reduce('+', result)/length(result)
+  names(which.max(clf["auc",]))
+}
+
+generate <- function(clf, size) {
+  data = window(read(FILES), size)
+  do.call(clf, list(data, data))
+}
+
+extract <- function(result, size) {
+
+  clf = best(result)
+  model = generate(clf, size)[[1]]
+  return(model)
+}
