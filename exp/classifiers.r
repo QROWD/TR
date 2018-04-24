fd24564153e632f1d6b90a65e388e934ddce9b2a
lpfgarcia <lpfgarcia@gmail.com>
2018-04-24 15:47:24 +0200

    rename and remove auc

--- exp/classifiers.r
+++ exp/classifiers.r
@@ -2,61 +2,53 @@
 # Luis P. F. Garcia 2018
 # Supervised Learning - Classifiers 
 
-ANN <- function(train, test) {
-  model = MLP(class ~ ., train)
+ANN <- function(tran, test) {
+  model = MLP(class ~ ., tran)
   pred = predict(model, test[,-ncol(test)], type="prob")
   list(model, pred)
 }
 
-C4.5 <- function(train, test) {
-  model = RWeka::J48(class ~ ., train)
+C4.5 <- function(tran, test) {
+  model = RWeka::J48(class ~ ., tran)
   pred = predict(model, test[,-ncol(test)], type="prob")
   list(model, pred)
 }
 
-kNN <- function(train, test, k=5) {
-  pred = kknn(class ~., train, test[,-ncol(test)], k=k)$prob
+kNN <- function(tran, test, k=5) {
+  pred = kknn(class ~., tran, test[,-ncol(test)], k=k)$prob
   rownames(pred) = rownames(test)
   list(NULL, pred)
 }
 
-NB <- function(train, test) {
-  model = naiveBayes(class ~ ., train)
+NB <- function(tran, test) {
+  model = naiveBayes(class ~ ., tran)
   pred = predict(model, test[,-ncol(test)], type="raw")
   rownames(pred) = rownames(test)
   list(model, pred)
 }
 
-RF <- function(train, test) {
-  model = randomForest(class ~ ., train)
+RF <- function(tran, test) {
+  model = randomForest(class ~ ., tran)
   pred = predict(model, test[,-ncol(test)], type="prob")
   list(model, pred)
 }
 
-SVM <- function(train, test) {
-  model = svm(class ~ ., train, kernel="radial", probability=TRUE)
+SVM <- function(tran, test) {
+  model = svm(class ~ ., tran, kernel="radial", probability=TRUE)
   pred = attr(predict(model, test[,-ncol(test)], probability=TRUE), "probabilities")
-  pred = pred[,levels(train$class)]
+  pred = pred[,levels(tran$class)]
   list(model, pred)
 }
 
-multiclass.auc <- function(pred, class) {
-  pred = apply(pred, 1, which.max)
-  as.numeric(multiclass.roc(class, pred)$auc)
-}
-
 accuracy <- function(pred, class) {
-  pred = factor(levels(class)[apply(pred, 1, which.max)], levels=levels(class))
-  diag(table(class, pred))/colSums(table(class, pred))
+  class = factor(class, levels=colnames(pred))
+  pred = factor(colnames(pred)[apply(pred, 1, which.max)], levels=colnames(pred))
+  diag(table(class, pred))/rowSums(table(class, pred))
 }
 
-performance <- function(pred, class) {
-  c(auc=multiclass.auc(pred, class), accuracy(pred, class))
-}
-
-classifiers <- function(train, test) {
+classifiers <- function(tran, test) {
   sapply(CLASSIFIERS, function(c) {
-    pred = do.call(c, list(train, test))[[2]]
-    performance(pred, test$class)
+    pred = do.call(c, list(tran, test))[[2]]
+    accuracy(pred, test$class)
   })
 }
