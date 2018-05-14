aa1560c3e2716fa8b93030931c6f9ea0a603b4fa
lpfgarcia <lpfgarcia@gmail.com>
2018-05-14 11:41:41 +0200

    accuracy and kappa measures

--- exp/performance.r
+++ exp/performance.r
@@ -2,6 +2,10 @@
 # Luis P. F. Garcia 2018
 # Evaluation measures
 
+accuracy <- function(true, pred) {
+  sum(diag(table(true, pred)))/sum(table(true, pred))
+}
+
 kappa <- function(true, pred) {
 
   aux = table(pred, true)
@@ -12,20 +16,11 @@ kappa <- function(true, pred) {
   return(aux)
 }
 
-accuracy <- function(true, pred) {
-  sum(diag(table(true, pred)))/sum(table(true, pred))
-}
-
-auc <- function(true, prob) {
-  as.numeric(multiclass.roc(true, apply(prob, 1, max))$auc)
-}
-
 performance <- function(true, prob) {
 
   clas = colnames(prob)
   true = factor(true, levels=clas)
   pred = factor(clas[apply(prob, 1, which.max)], levels=clas)
-
-  c(kappa=kappa(true, pred), acc=accuracy(true, pred),
+  c(acc=accuracy(true, pred), kappa=kappa(true, pred),
     auc=auc(true, prob))
 }
