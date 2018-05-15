6c52faf4c93af57c5d8e43525440c30fee35b63e
lpfgarcia <lpfgarcia@gmail.com>
2018-05-15 16:59:16 +0200

    add f1

--- exp/performance.r
+++ exp/performance.r
@@ -16,10 +16,17 @@ kappa <- function(true, pred) {
   return(aux)
 }
 
+f1 <- function(true, pred) {
+  p = diag(table(true, pred))/colSums(table(true, pred))
+  r = diag(table(true, pred))/rowSums(table(true, pred))
+  (2 * p * r) / (p + r)
+}
+
 performance <- function(true, prob) {
 
   clas = colnames(prob)
   true = factor(true, levels=clas)
   pred = factor(clas[apply(prob, 1, which.max)], levels=clas)
-  c(acc=accuracy(true, pred), kappa=kappa(true, pred))
+  c(acc=accuracy(true, pred), kappa=kappa(true, pred),
+    f1=mean(f1(true, pred)))
 }
