f959ca28ed6be211e14b419bff7c3a6c99b0159a
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2018-05-24 11:53:09 +0200

    server with CLI args

--- prediction_server.R
+++ prediction_server.R
@@ -1,3 +1,23 @@
+library("optparse")
+
+option_list = list(
+  make_option(c("", "--host"), type="character", default="localhost", 
+              help="hostname [default= %default]", metavar="ADDRESS"),
+  make_option(c("-p", "--port"), type="integer", default=6011, 
+              help="port number [default= %default]", metavar="NUMBER"),
+  make_option(c("-m", "--model"), type="character", default=NULL, 
+              help="path to model [default= %default]", metavar="path")
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$model)){
+  print_help(opt_parser)
+  stop("Path to model file must be given!", call.=FALSE)
+}
+
+
 load <- function() {
   aux = list.files("exp", full.name=TRUE) 
   for(file in aux) source(file);
@@ -5,20 +25,14 @@ load <- function() {
 
 load()
 
-args = commandArgs(trailingOnly=TRUE)
-
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("model file hasto be provided (input file).n", call.=FALSE)
-}
 
 # init ML model
-init_model(args[1])
+init_model(opt$model)
 
 server <- function(){
-  writeLines("Listening...")
+  writeLines(sprintf("Listening to port %d", opt$port))
   while(TRUE){
-    con <- socketConnection(host="localhost", port = 6011, blocking=TRUE,
+    con <- socketConnection(host=opt$host, port = opt$port, blocking=TRUE,
                             server=TRUE, open="r+")
     data <- readLines(con, 1)
    
